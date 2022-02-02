0c2510b6c2e6117ded575ca878a0af443d724ceb
Felix Nithammer <f.nithammer@mammutmedia.eu>
2022-02-02 22:32:23 +0100

    Initial Commit

---
+++ ar-community-swiftui/ar_community_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_community_swiftuiApp.swift
+//  ar-community-swiftui
+//
+//  Created by Mammut Nithammer on 02.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ar_community_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
